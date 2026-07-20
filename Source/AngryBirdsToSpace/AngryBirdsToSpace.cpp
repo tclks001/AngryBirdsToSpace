451ad8f60946d910005c0b6df495f2d44bfe7474
mingyangwu <mingyangwu@tencent.com>
2026-07-20 18:05:21 +0800

    Initial commit

---
+++ Source/AngryBirdsToSpace/AngryBirdsToSpace.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AngryBirdsToSpace.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AngryBirdsToSpace, "AngryBirdsToSpace" );
